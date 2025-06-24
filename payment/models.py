f9772a346128650a417264bf9d3a26c19851d1e9
Gaurav mishra <csegaurav0@gmail.com>
2025-06-24 13:53:34 +0530

    create shiping address at the time user creation using signals

--- payment/models.py
+++ payment/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from store.models import Product
+from django.db.models.signals import post_save
 
 
 class ShippingAddress(models.Model):
@@ -22,6 +23,15 @@ class ShippingAddress(models.Model):
 
 	def __str__(self):
 		return f'Shipping Address - {str(self.id)}'
+	
+# Create a user Shipping Address by default when user signs upAdd commentMore actions
+def create_shipping(sender, instance, created, **kwargs):
+	if created:
+		user_shipping = ShippingAddress(user=instance)
+		user_shipping.save()
+
+# Automate the profile thing
+post_save.connect(create_shipping, sender=User)
 	
 
 class Order(models.Model):
