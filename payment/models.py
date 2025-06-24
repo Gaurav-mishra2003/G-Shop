d59c76f2d10faf1294c7036d3d7fe7d6c3382b15
Gaurav mishra <csegaurav0@gmail.com>
2025-06-24 14:35:27 +0530

    shipped date

--- payment/models.py
+++ payment/models.py
@@ -2,36 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from store.models import Product
-from django.db.models.signals import post_save
-
-
-class ShippingAddress(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-	shipping_full_name = models.CharField(max_length=255)
-	shipping_email = models.CharField(max_length=255)
-	shipping_address1 = models.CharField(max_length=255)
-	shipping_address2 = models.CharField(max_length=255,null=True, blank=True)
-	shipping_city = models.CharField(max_length=255)
-	shipping_state = models.CharField(max_length=255, null=True, blank=True)
-	shipping_zipcode = models.CharField(max_length=255, null=True, blank=True)
-	shipping_country = models.CharField(max_length=255)
-
-
-	# Don't pluralize address
-	# class Meta:
-	# 	verbose_name_plural = "Shipping Address"
-
-	def __str__(self):
-		return f'Shipping Address - {str(self.id)}'
-	
-# Create a user Shipping Address by default when user signs upAdd commentMore actions
-def create_shipping(sender, instance, created, **kwargs):
-	if created:
-		user_shipping = ShippingAddress(user=instance)
-		user_shipping.save()
-
-# Automate the profile thing
-post_save.connect(create_shipping, sender=User)
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+import datetime
 	
 
 class Order(models.Model):
@@ -46,6 +19,46 @@ class Order(models.Model):
 
 	def __str__(self):
 		return f'Order - {str(self.id)}'
+
+
+class ShippingAddress(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+	shipping_full_name = models.CharField(max_length=255)
+	shipping_email = models.CharField(max_length=255)
+	shipping_address1 = models.CharField(max_length=255)
+	shipping_address2 = models.CharField(max_length=255,null=True, blank=True)
+	shipping_city = models.CharField(max_length=255)
+	shipping_state = models.CharField(max_length=255, null=True, blank=True)
+	shipping_zipcode = models.CharField(max_length=255, null=True, blank=True)
+	shipping_country = models.CharField(max_length=255)
+	date_shipped = models.DateTimeField(blank=True, null=True)
+
+
+	# Don't pluralize address
+	# class Meta:
+	# 	verbose_name_plural = "Shipping Address"
+
+	def __str__(self):
+		return f'Shipping Address - {str(self.id)}'
+	
+# Auto Add shipping DateAdd commentMore actions
+@receiver(pre_save, sender=Order)
+def set_shipped_date_on_update(sender, instance, **kwargs):
+	if instance.pk:
+		now = datetime.datetime.now()
+		obj = sender._default_manager.get(pk=instance.pk)
+		if instance.shipped and not obj.shipped:
+			instance.date_shipped = now
+
+	
+# Create a user Shipping Address by default when user signs upAdd commentMore actions
+def create_shipping(sender, instance, created, **kwargs):
+	if created:
+		user_shipping = ShippingAddress(user=instance)
+		user_shipping.save()
+
+# Automate the profile thing
+post_save.connect(create_shipping, sender=User)
 
 # Create Order Items Model
 class OrderItem(models.Model):
