60f435d1741dc01d95f85cb7b4259faf19ddeeef
Gaurav mishra <csegaurav0@gmail.com>
2025-06-24 14:22:09 +0530

    order items

--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,99 @@
 from django.shortcuts import render,redirect
 from cart.cart import Cart
 from payment.forms import ShippingForm, PaymentForm
-from payment.models import ShippingAddress
+from payment.models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 
+
+def process_order(request):
+	if request.POST:
+		# Get the cart
+		cart = Cart(request)
+		cart_products = cart.get_prods
+		quantities = cart.get_quants
+		totals = cart.cart_total()
+
+		# Get Billing Info from the last page
+		payment_form = PaymentForm(request.POST or None)
+		# Get Shipping Session Data
+		my_shipping = request.session.get('my_shipping')
+
+		# Gather Order Info
+		full_name = my_shipping['shipping_full_name']
+		email = my_shipping['shipping_email']
+		# Create Shipping Address from session info
+		shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}"
+		amount_paid = totals
+
+		# Create an Order
+		if request.user.is_authenticated:
+			# logged in
+			user = request.user
+			# Create Order
+			create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+			create_order.save()
+
+						# Add order itemsAdd commentMore actions
+			
+			# Get the order ID
+			order_id = create_order.pk
+			
+			# Get product Info
+			for product in cart_products():
+				# Get product ID
+				product_id = product.id
+				# Get product price
+				if product.is_sale:
+					price = product.sale_price
+				else:
+					price = product.price
+
+				# Get quantity
+				for key,value in quantities().items():
+					if int(key) == product.id:
+						# Create order item
+						create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+						create_order_item.save()
+
+			messages.success(request, "Order Placed!")
+			return redirect('home')
+
+		else:
+			# not logged in
+			# Create Order
+			create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+			create_order.save()
+				# Add order itemsAdd commentMore actions
+			
+			# Get the order ID
+			order_id = create_order.pk
+			
+			# Get product Info
+			for product in cart_products():
+				# Get product ID
+				product_id = product.id
+				# Get product price
+				if product.is_sale:
+					price = product.sale_price
+				else:
+					price = product.price
+
+				# Get quantity
+				for key,value in quantities().items():
+					if int(key) == product.id:
+						# Create order item
+						create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+						create_order_item.save()
+
+			messages.success(request, "Order Placed!")
+			return redirect('home')
+
+
+	else:
+		messages.success(request, "Access Denied")
+		return redirect('home')
 
 def billing_info(request):
 	if request.POST:
@@ -13,6 +102,9 @@ def billing_info(request):
 		cart_products = cart.get_prods
 		quantities = cart.get_quants
 		totals = cart.cart_total()
+		# Create a session with Shipping InfoAdd commentMore actions
+		my_shipping = request.POST
+		request.session['my_shipping'] = my_shipping
 
 		# Check to see if user is logged in
 		if request.user.is_authenticated:
